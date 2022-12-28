679b060a42829f0e5bf2eca97d61d45d84148e9a
shamskhodary <shams.khodary@gmail.com>
2022-12-28 12:56:29 +0200

    make dropDown in the navbar relates #28

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,15 +1,77 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Input } from 'antd'
+import {
+  Avatar, Typography,
+  Button, Dropdown, Input, MenuProps, Space,
+} from 'antd'
+import { DownOutlined } from '@ant-design/icons'
 import logo from '../assets/logo.png'
 import '../styles/navbar.css'
 import { Signin, Signup } from '../pages'
 import { useAuth } from '../context/authUser'
 
 const { Search } = Input
+const { Title } = Typography
 
 const Navbar:FC = () => {
   const auth = useAuth()
+  const items: MenuProps['items'] = [
+    {
+      label: (
+        <Button
+          type="primary"
+          style={{
+            backgroundColor: 'transparent',
+            border: 0,
+            boxShadow: 'none',
+            width: '100%',
+            fontWeight: 700,
+            color: '#000',
+          }}
+        >
+          Profile
+        </Button>
+      ),
+      key: 0,
+    },
+    {
+      label: (
+        <Button
+          type="primary"
+          style={{
+            backgroundColor: 'transparent',
+            border: 0,
+            boxShadow: 'none',
+            width: '100%',
+            fontWeight: 700,
+            color: '#000',
+          }}
+        >
+          Saves
+        </Button>
+      ),
+      key: 1,
+    },
+    {
+      label: (
+        <Button
+          type="primary"
+          style={{
+            backgroundColor: 'transparent',
+            border: 0,
+            boxShadow: 'none',
+            width: '100%',
+            fontWeight: 700,
+            color: '#000',
+          }}
+        >
+          Log out
+        </Button>
+      ),
+      key: 2,
+    },
+  ]
+
   return (
     <header>
       <div className="logo">
@@ -31,20 +93,27 @@ const Navbar:FC = () => {
           </>
 
         ) : (
-          <Button
-            type="primary"
-            style={{
-              backgroundColor: '#edeff1',
-              borderRadius: '1.5rem',
-              padding: '0.3rem 2rem',
-              height: 'auto',
-              fontWeight: 700,
-              color: '#000',
-            }}
-          >
-            Log out
+          <Dropdown menu={{ items }} trigger={['click']}>
+            <button
+              type="button"
+              onClick={(e) => e.preventDefault()}
+              style={{
+                border: 0,
+                backgroundColor: 'transparent',
+                cursor: 'pointer',
+              }}
+            >
+              <Space style={{
+                alignItems: 'baseline',
+              }}
+              >
+                <Avatar style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}>U</Avatar>
+                <Title level={5}>username</Title>
+                <DownOutlined />
+              </Space>
+            </button>
+          </Dropdown>
 
-          </Button>
         )}
 
       </div>
